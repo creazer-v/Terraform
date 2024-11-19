a1c441a89df8ea8f958d6a91bef14ac87ae17289
Mahendra <mahendra.m2@carelon.com>
2024-11-19 12:57:12 +0530

    created by

---
+++ main.tf
@@ -0,0 +1,15 @@
+variable "business-division" {
+  type        = string
+  description = "The business division that is creating the resources"
+  default = "corp"
+  
+}
+
+#####################################################################################################################################
+##			                                          CreatedBy Provisioner                                                               
+#####################################################################################################################################
+module "created-by" {
+  source = "cps-terraform.anthem.com/corp/terraform-aws-createdby/aws"
+
+  business-division = var.business-division
+}
